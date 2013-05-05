d46fbeec8c9f4a3a956d3496320f04778a87cc60
Krozark <maxime.barbier1991@gmail.com>
2013-05-05 17:13:47 +0200

    add file for easy include

---
+++ src/event.hpp
@@ -0,0 +1,8 @@
+#include "event/BaseEventCallable.hpp"
+
+#include "event/EventCallback.hpp"
+#include "event/EventCallbackEvent.hpp"
+
+#include "event/EventObject.hpp"
+#include "event/EventObjectEvent.hpp"
+
